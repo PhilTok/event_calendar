a5d72739c25bb22f66ef409bc2f73c2f8a911554
Phil <tokarev.ph@gmail.com>
2019-07-12 16:47:00 +0300

    Add delete, edit, create function Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+
 
 	def show
 		@event = Event.find(params[:id])
@@ -44,5 +47,10 @@ class EventsController < ApplicationController
 
     def event_params
       params.require(:event).permit(:name, :content, :datetime, :repeat)
+    end
+
+    def correct_user
+      @event = current_user.events.find_by(id: params[:id])
+      redirect_to root_url if @event.nil?
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get    'login', to: 'sessions#new'
   post   'login', to: 'sessions#create'
   delete 'logout',to: 'sessions#destroy'
+  get 'create',   to: 'events#new'
   resources :users
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :events
